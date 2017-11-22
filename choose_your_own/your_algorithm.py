cc5336d79f0f4d9925424b9456c0d96fc5c319f7
Michael Diente <kheldiente@gmail.com>
2017-11-22 20:06:41 +0800

    Finished testing algorithms (KNN, Adaboost, Random Forest)
    There are pictures of drawn decision boundaries in the ./img in each algorithm tested

--- choose_your_own/your_algorithm.py
+++ choose_your_own/your_algorithm.py
@@ -3,6 +3,7 @@
 import matplotlib.pyplot as plt
 from prep_terrain_data import makeTerrainData
 from class_vis import prettyPicture
+from time import time
 
 features_train, labels_train, features_test, labels_test = makeTerrainData()
 
@@ -30,12 +31,41 @@ plt.show()
 
 ### your code here!  name your classifier object clf if you want the
 ### visualization code (prettyPicture) to show you the decision boundary
-
+print "number of features_train: ", len(features_train)
+print "numbe rof labels_train: ", len(labels_train)
 ### k nearest neighbors classifier ###
-from sklearn.neighbors import KNeighborsClassifier
-clf = KNeighborsClassifier(n_neighbors=3)
+# from sklearn.neighbors import KNeighborsClassifier
+# clf = KNeighborsClassifier(n_neighbors=3)
+# print "\n###executing k nearest neighbor classifier test###\n"
+# t0 = time()
+# clf = clf.fit(features_train, labels_train)
+# print "training time: ", round(time() - t0, 3), "s"
+# t0 = time()
+# pred = clf.predict(features_test)
+# print "prediction time: ", round(time() - t0, 3), "s"
+
+### adaboost classifier ###
+# from sklearn.ensemble import AdaBoostClassifier
+# clf = AdaBoostClassifier()
+# print "\n###executing adaboost classifier test###\n"
+# t0 = time()
+# clf = clf.fit(features_train, labels_train)
+# print "training time: ", round(time() - t0, 3), "s"
+# t0 = time()
+# pred = clf.predict(features_test)
+# print "prediction time: ", round(time() - t0, 3), "s"
+
+### random forest classifier ###
+from sklearn.ensemble import RandomForestClassifier
+clf = RandomForestClassifier()
+print "\n###executing random forest classifier test###\n"
+t0 = time()
 clf = clf.fit(features_train, labels_train)
+print "traing time: ", round(time() - t0, 3), "s"
+t0 = time()
 pred = clf.predict(features_test)
+print "prediction time: ", round(time() - t0, 3), "s"
+
 from sklearn.metrics import accuracy_score
 print "accuracy score: ", accuracy_score(labels_test, pred)
 try:
